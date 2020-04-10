e072ce0a06881d92d3d04516e2e5cd1d4e46541e
aleksey4uk <aleksey4uk@gmail.com>
2020-04-10 07:39:58 +0300

    refactoring code

---
+++ build/precache-manifest.86b82f900c7ea1b4b036860d8a88ef7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5300113d642b3263c12c5d2d3fa02d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed6b8183e07b5500354",
+    "url": "/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "dcf6cfa926aec56d7575",
+    "url": "/static/css/main.049549c4.chunk.css"
+  },
+  {
+    "revision": "5ed6b8183e07b5500354",
+    "url": "/static/js/2.05d0360f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.05d0360f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf6cfa926aec56d7575",
+    "url": "/static/js/main.a13d5485.chunk.js"
+  },
+  {
+    "revision": "c0e51996894ebd34b9fb",
+    "url": "/static/js/runtime-main.83ca3af7.js"
+  }
+]);
